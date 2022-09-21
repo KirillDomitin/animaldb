abb06dc6e4e0930c1619ec7b12128e5b4bd02608
kirill <domitin@gmail.com>
2022-09-22 01:40:14 +0300

    add crud for animals

--- animals/urls.py
+++ animals/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import AnimalListView, AnimalCreateView
+from .views import AnimalListView, AnimalCreateView, AnimalUpdateView, AnimalDeleteView, AnimalDetailView
 
 urlpatterns = [
     path('', AnimalListView.as_view(), name='animal_list'),
     path('create/', AnimalCreateView.as_view(), name='animal_create'),
+    path('<int:pk>/update/', AnimalUpdateView.as_view(), name='animal_update'),
+    path('<int:pk>/delete/', AnimalDeleteView.as_view(), name='animal_delete'),
+    path('<int:pk>/', AnimalDetailView.as_view(), name='animal_detail'),
 ]
--- animals/views.py
+++ animals/views.py
@@ -1,22 +1,42 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 
 from .models import AnimalModel, ShelterModel
-from django.views.generic import ListView, DetailView, UpdateView, DeleteView, CreateView
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView, CreateView, RedirectView
 from .forms import AnimalCreateForm
 
 
 class AnimalListView(ListView):
     model = AnimalModel
-    template_name = 'animals/list.html'
+    template_name = 'animals/animal_list.html'
     context_object_name = 'animal_list'
 
     def get_queryset(self):
-        return AnimalModel.objects.all()
+        return AnimalModel.objects.order_by('pk').all()
 
 
 class AnimalCreateView(CreateView):
     form_class = AnimalCreateForm
-    template_name = 'animals/create.html'
+    template_name = 'animals/animal_create.html'
     context_object_name = 'animal_create'
     success_url = reverse_lazy('animal_list')
+
+
+class AnimalUpdateView(UpdateView):
+    model = AnimalModel
+    template_name = 'animals/animal_update.html'
+    context_object_name = 'animal_update'
+    fields = '__all__'
+    success_url = reverse_lazy('animal_list')
+
+class AnimalDeleteView(DeleteView):
+    model = AnimalModel
+    template_name = 'animals/animal_delete.html'
+    context_object_name = 'animal_delete'
+    success_url = reverse_lazy('animal_list')
+
+class AnimalDetailView(DetailView):
+    model = AnimalModel
+    template_name = 'animals/animal_detail.html'
+    context_object_name = 'animal_detail'
